b7fcfa70f82fd9fb34774eb1f19d6edc87f05713
Rafael Lopez <lpz.raf@gmail.com>
2020-10-09 22:26:20 -0400

    Fixed additional flake8 warning in the forms.py file.

--- forms.py
+++ forms.py
@@ -1,16 +1,20 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, validators
 
+
 class UserForm(FlaskForm):
     username = StringField('Username', [validators.DataRequired()])
     password = PasswordField('Password', [validators.DataRequired()])
+
 
 class NoteForm(FlaskForm):
     title = StringField('Title', [validators.DataRequired()])
     note_body = StringField('Note Body', [validators.DataRequired()])
 
+
 class DeleteForm(FlaskForm):
     pass
+
 
 class LoginForm(FlaskForm):
     username = StringField('Username', [validators.DataRequired()])
